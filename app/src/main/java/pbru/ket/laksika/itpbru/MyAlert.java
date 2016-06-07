8baa5260b2f430354a9470fcd5b358a060d0ee7f
laksikaket <eveferkafew@mail.com>
2016-06-07 11:10:01 +0700

    ส้ราง popup

---
+++ app/src/main/java/pbru/ket/laksika/itpbru/MyAlert.java
@@ -0,0 +1,33 @@
+package pbru.ket.laksika.itpbru;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+/**
+ * Created by lap324-11 on 6/7/2016 AD.
+ */
+public class MyAlert {
+
+    public void  myDialog(Context context,
+                          String strTitle,
+                          String strMessage ) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setCancelable(false);
+        builder.setIcon(R.drawable.doremon48);
+        builder.setTitle(strTitle);
+        builder.setMessage(strMessage);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.show();
+
+    }   //myDialog
+
+
+
+}   //My Class
